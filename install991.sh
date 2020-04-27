0ebf320424035fa00f70227cc04de28b2186c048
rjn991 <ranjanht991@gmail.com>
2020-04-27 20:48:38 +0530

    update

--- install991.sh
+++ install991.sh
@@ -8,9 +8,6 @@ apt install python
 echo "updating pip3"
 pip install --upgrade pip
 
-echo "Downloading youtube-dl"
-pip3 install youtube-dl
-
 echo "Downloading spotdl"
 pip3 install spotdl
 
--- install992.sh
+++ install992.sh
@@ -1,18 +1,27 @@
 #rjn991
 echo "Creating folders"
+#bin
 mkdir -p ~/bin
+#config
 mkdir -p ~/.config/youtube-dl
 mkdir -p ~/.config/spotdl
+#internalstorage/termux
 mkdir -p ~/storage/shared/termux/youtube
 mkdir -p ~/storage/shared/termux/spotify
-mkdir -p ~/storage/shared/termux/spotify
+mkdir -p ~/storage/shared/termux/music
 
 echo "Copying files"
+#youtube-dl
 cp ~/ydlsdl/youtubedl/config/config ~/.config/youtube-dl
+#spotdl
 rm -rf ~/.config/spotdl/config
-cp ~/ydlsdl/spotdl/config/config ~/.config/spotdl
+cp ~/ydlsdl/spotdl/config/config.yml ~/.config/spotdl
+#termux-url-opener
 cp ~/ydlsdl/termux-url-opener ~/bin
+#spotif-file
+rm -rf /data/data/com.termux/files/usr/lib/python3.8/site-packages/spotdl/spotify_tools.py
 
+cp ~/ydlsdl/spotdl/file/spotify_tools.py /data/data/com.termux/files/usr/lib/python3.8/site-packages/spotdl
 
 echo "Making files executable"
 
